b92ec3bb42e83372ff62ac2a3362aad5a132a58b
Shamsutdinov Vladislav <okomsky@yandex.ru>
2023-07-25 00:20:21 +1100

    не понимаю как сделать четвертое задание =(

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.*;
 
+public class Main {
+
+    private static final List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+    private static final List<String> strings = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
+    private static final List<String> words = new ArrayList<>(List.of("horse", "dog","horse", "cat", "desk", "desk", "fall", "a", "a"));
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
     }
+
+    public static void task1() {
+        for (int num : nums) {
+            if (num % 2 != 0) {
+                System.out.println(num);
+            }
+        }
+    }
+
+    public static void task2() {
+        Collections.sort(nums);
+        int uniqueNum = Integer.MIN_VALUE;
+        for (int num : nums) {
+            if (num % 2 == 0 && num != uniqueNum) {
+                System.out.println(num);
+                uniqueNum = num;
+            }
+        }
+    }
+
+    public static void task3() {
+        Set<String> uniqueWords = new HashSet<>(words);
+        System.out.println(uniqueWords);
+    }
+
+
 }
